43f4208d933ba64402b01324c0dc842a43ca03a1
Frostlynx <frostlynx6@gmail.com>
2026-07-25 02:05:13 +0800

    Fix Prisma typescript error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,8 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+// Prisma 7 automatically handles the URL via your config file!
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
